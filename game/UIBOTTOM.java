d2edab97f614e2a57a3b989a6c5ddb98831ca2d1
SimonDiabolic <simon-goerke@gmx.net>
2021-06-30 09:55:23 +0200

    retrybutton im spiel

--- game/UIBOTTOM.java
+++ game/UIBOTTOM.java
@@ -3,6 +3,7 @@ package game;
 import java.awt.image.BufferedImage;
 import java.awt.Graphics;
 import java.text.DecimalFormat;
+import java.awt.Font;
 
 //---------------------//
 public class UIBOTTOM
@@ -12,14 +13,22 @@ public class UIBOTTOM
     private BufferedImage l1;
     private HERZ[] herzen;
     int i = 0;
+    private BUTTON[] buttons;
+    private Font f;
     public UIBOTTOM(int x, int y)
     {
         this.x = x; 
         this.y = y;
         l1 = TEXTUR.bar;
+        f = new Font("Monospaced",Font.BOLD + Font.ITALIC/*Fett/Kursiv*/,25/*Schriftgröße*/);
+        
+        BufferedImage[] texturen1 = {TEXTUR.retry, TEXTUR.retry1, TEXTUR.retry1};
         BufferedImage[] HerzZustand = {TEXTUR.hr, TEXTUR.hs};
         herzen = new HERZ[5];
-
+        
+        buttons =  new BUTTON[1];
+        buttons[0] = new BUTTON(350,825, "", texturen1, f);
+        
         herzen[0] = new HERZ(440,825, HerzZustand); 
         herzen[1] = new HERZ(500,825, HerzZustand); 
         herzen[2] = new HERZ(560,825, HerzZustand); 
@@ -30,10 +39,24 @@ public class UIBOTTOM
     public void draw(Graphics g)
     {
       g.drawImage(l1, x, y, null);
+        for(int i = 0; i < buttons.length;i++)
+      {
+          buttons[i].draw(g);
+      }
         for(int i = 0; i < herzen.length;i++)
       {
           herzen[i].draw(g);
       }
+    }
+    public void update()
+    {
+            for(int i = 0; i < buttons.length;i++)
+      {
+          if(buttons[i].update())
+          {
+            if (i == 1){FRAME.spielzustand = 2;FRAME.fmenu = null;}
+          }
+      } 
     }
     public void lebenNehmen()
     {
