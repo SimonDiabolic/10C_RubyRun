c1cb3293126a0072ccb32eb68249902a27baf4f0
TimTischler-05 <Timtischler2000@gmail.com>
2021-07-08 12:16:22 +0200

    bla

--- game/SCHLUESSEL.java
+++ game/SCHLUESSEL.java
@@ -3,34 +3,34 @@ import java.awt.image.BufferedImage;
 import java.awt.Graphics;
 import java.util.LinkedList;
 
-//public class SCHLUESSEL extends BEWEGTESOBJEKT
-//{
-//  SCHLUESSEL (int x, int y)
-//  {
-//      super(x,y);
-//      look = TEXTUR.schluessel;
+/*public class SCHLUESSEL extends BEWEGTESOBJEKT
+{
+  SCHLUESSEL (int x, int y)
+  {
+      super(x,y);
+      look = TEXTUR.schluessel;
         
-//   }
-//  public void update()
-//   {
-//       super.update();
-//  }
-//  public void draw(Graphics g)
-//  {
-//   g.drawImage(look, x, y, null);
-//  }
-//  public int getRubinX() { return x; }
-//  public int getRubinY() { return y; }
-//   public boolean SchluesselCollection()
-//   {
-//       if ((getX()==SPIELER.getXPos()) && (getY()==SPIELER.getYPos()))
-//       {
-//           return true;
-//      }
-//      else
-//       {
-//           return false;
-//       }
-//   }
-//}
+   }
+  public void update()
+   {
+       super.update();
+  }
+  public void draw(Graphics g)
+  {
+   g.drawImage(look, x, y, null);
+  }
+  public int getRubinX() { return x; }
+  public int getRubinY() { return y; }
+   public boolean SchluesselCollection()
+   {
+       if ((getX()==SPIELER.getXPos()) && (getY()==SPIELER.getYPos()))
+       {
+          return true;
+      }
+      else
+       {
+           return false;
+       }
+   }
+}*/
 
