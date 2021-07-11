5d88f40116c7038b500777bd0d8f26e34084beab
LouSnoo <nina.marthedal@gmail.com>
2021-07-11 14:33:08 +0200

    Tutorial FERTIG

--- game/TUTORIAL.java
+++ game/TUTORIAL.java
@@ -22,6 +22,15 @@ public class TUTORIAL
     private boolean drawWayBlock;
     private boolean FirstWayBlock;
     private boolean drawWASD;
+    private boolean drawT1;
+    private boolean drawT2;
+    private boolean drawT3;
+    private boolean drawT4;
+    private boolean drawT5;
+    private boolean drawT6;
+    private boolean drawT7;
+    private boolean drawT8;
+    private boolean drawT9;
     public TUTORIAL()
     {
         WASD = TEXTUR.WASD;
@@ -36,6 +45,15 @@ public class TUTORIAL
         T9 = TEXTUR.T9;
         FirstWayBlock = true;
         drawWASD = true;
+        drawT2 = true;
+        drawT3 = true;
+        drawT4 = true;
+        drawT5 = true;
+        drawT6 = true;
+        drawT7 = true;
+        drawT8 = true;
+        drawT9 = true;
+        drawWayBlock = true;
     }
     public void draw(Graphics g)
     {
@@ -48,45 +66,106 @@ public class TUTORIAL
                     {
                         drawWASD = false;
                     }
-                },1000);
-                
-                
+                },2500);
       }
-      if(SPIELER.getXPos()==320 && SPIELER.getYPos() == 80)
+      if(SPIELER.getXPos()==320 && SPIELER.getYPos() == 80 && drawT2)
       {
+          Timer timer = new Timer();
           g.drawImage(T2, 800/2 - T2.getWidth()/2, 700, null);
+          timer.schedule(new TimerTask(){
+                    public void run()
+                    {
+                        drawT2 = false;
+                    }
+                },2500);
       }
-      if(SPIELER.getXPos()==440 && SPIELER.getYPos() == 80)
+      if(SPIELER.getXPos()==440 && SPIELER.getYPos() == 80 && drawT3)
       {
+          Timer timer = new Timer();
           g.drawImage(T3, 800/2 - T3.getWidth()/2, 700, null);
+          timer.schedule(new TimerTask(){
+                    public void run()
+                    {
+                        drawT3 = false;
+                    }
+                },2500);
       }
-      if(SPIELER.getXPos()==400 && SPIELER.getYPos() == 240)
+      if(SPIELER.getXPos()==400 && SPIELER.getYPos() == 240 && drawT4)
       {
+          Timer timer = new Timer();
           g.drawImage(T4, 800/2 - T4.getWidth()/2, 700, null);
+          timer.schedule(new TimerTask(){
+                    public void run()
+                    {
+                        drawT4 = false;
+                    }
+                },2500);
       }
-      if(SPIELER.getXPos()==360 && SPIELER.getYPos() == 240)
+      if(SPIELER.getXPos()==360 && SPIELER.getYPos() == 240 && drawT5)
       {
+          Timer timer = new Timer();
           g.drawImage(T5, 800/2 - T5.getWidth()/2, 700, null);
+          timer.schedule(new TimerTask(){
+                    public void run()
+                    {
+                        drawT5 = false;
+                    }
+                },2500);
       }
-      if(SPIELER.getXPos()==520 && SPIELER.getYPos() == 400)
+      if(SPIELER.getXPos()==520 && SPIELER.getYPos() == 400 && drawT6)
       {
+          Timer timer = new Timer();
           g.drawImage(T6, 800/2 - T6.getWidth()/2, 700, null);
+          timer.schedule(new TimerTask(){
+                    public void run()
+                    {
+                        drawWASD = false;
+                    }
+                },2500);
       }
-      if(SPIELER.getXPos()==480 && SPIELER.getYPos() == 560)
+      if(SPIELER.getXPos()==480 && SPIELER.getYPos() == 560 && drawWayBlock)
       {
+          Timer timer = new Timer();
           g.drawImage(WayBlock, 800/2 - WayBlock.getWidth()/2, 700, null);
+          timer.schedule(new TimerTask(){
+                    public void run()
+                    {
+                        drawWayBlock = false;
+                    }
+                },2500);
       }
-      if(SPIELER.getXPos()==360 && SPIELER.getYPos() == 640)
+      if(SPIELER.getXPos()==360 && SPIELER.getYPos() == 640 && drawT7)
       {
+          Timer timer = new Timer();
           g.drawImage(T7, 800/2 - T7.getWidth()/2, 700, null);
+          timer.schedule(new TimerTask(){
+                    public void run()
+                    {
+                        drawT7 = false;
+                    }
+                },2500);
       }
-      if(SPIELER.getXPos()==320 && SPIELER.getYPos() == 640)
+      if(SPIELER.getXPos()==320 && SPIELER.getYPos() == 640 && drawT8)
       {
+          Timer timer = new Timer();
           g.drawImage(T8, 800/2 - T8.getWidth()/2, 700, null);
+          timer.schedule(new TimerTask(){
+                    public void run()
+                    {
+                        drawT8 = false;
+                    }
+                },2500);
       }
-      if(SPIELER.getXPos()==120 && SPIELER.getYPos() == 640)
+      if(SPIELER.getXPos()==120 && SPIELER.getYPos() == 640 && drawT9)
       {
+          Timer timer = new Timer();
           g.drawImage(T9, 800/2 - T9.getWidth()/2, 700, null);
+          timer.schedule(new TimerTask(){
+                    public void run()
+                    {
+                        drawT9 = false;
+                    }
+                },2500);
       }
     
     }
