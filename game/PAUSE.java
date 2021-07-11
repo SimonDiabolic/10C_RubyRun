4a88e817cc64817d5c6d8a0ed8cb863de9bbcb46
SimonDiabolic <simon-goerke@gmx.net>
2021-07-11 13:08:47 +0200

    tf

--- game/PAUSE.java
+++ game/PAUSE.java
@@ -33,15 +33,17 @@ public class PAUSE
         background = game.TEXTUR.background;
         pause = game.TEXTUR.pause;
         perfekt = TEXTUR.perfekt;
-        BufferedImage[] texturen1 = {TEXTUR.resume, TEXTUR.resume1, TEXTUR.resume1};
-        BufferedImage[] texturen2 = {TEXTUR.goMenu, TEXTUR.goMenu1, TEXTUR.goMenu1};
+        BufferedImage[] texturen1 = {TEXTUR.retry, TEXTUR.retry1, TEXTUR.retry1};
+        BufferedImage[] texturen2 = {TEXTUR.goMenu, TEXTUR.goMenu1, TEXTUR.goMenu2};
+        BufferedImage[] texturen3 = {TEXTUR.next, TEXTUR.next1, TEXTUR.next2};
   
         f = new Font("Monospaced",Font.BOLD + Font.ITALIC/*Fett/Kursiv*/,25/*Schriftgröße*/);
         font = new Font("Monospaced",Font.BOLD/*Fett/Kursiv*/,30/*Schriftgröße*/);
         
         buttons = new BUTTON[2];
         buttons[0] = new BUTTON(126, 730, "", texturen1, f);
-        buttons[1] = new BUTTON(545, 730, "", texturen2, f);
+        buttons[1] = new BUTTON(330, 730, "", texturen2, f);
+        buttons[2] = new BUTTON(545, 730, "", texturen3, f);
         
         rubine = "Rubine: " +WELT.punkte +"/" + WELT.anzahlRubine;
         saphire = "Saphire: " +LEVELWAHL.saphire +"/" + WELT.anzahlSaphire;
--- game/TUTORIAL.java
+++ game/TUTORIAL.java
@@ -10,6 +10,14 @@ import java.util.TimerTask;
 public class TUTORIAL
 {
     private BufferedImage WASD;
+    private BufferedImage T2;
+    private BufferedImage T3;
+    private BufferedImage T4;
+    private BufferedImage T5;
+    private BufferedImage T6;
+    private BufferedImage T7;
+    private BufferedImage T8;
+    private BufferedImage T9;
     private BufferedImage WayBlock;
     private boolean drawWayBlock;
     private boolean FirstWayBlock;
@@ -17,6 +25,14 @@ public class TUTORIAL
     {
         WASD = TEXTUR.WASD;
         WayBlock = TEXTUR.WayBlock;
+        T2 = TEXTUR.T2;
+        T3 = TEXTUR.T3;
+        T4 = TEXTUR.T4;
+        T5 = TEXTUR.T5;
+        T6 = TEXTUR.T6;
+        T7 = TEXTUR.T7;
+        T8 = TEXTUR.T8;
+        T9 = TEXTUR.T9;
         FirstWayBlock = true;
     }
     public void draw(Graphics g)
@@ -25,37 +41,41 @@ public class TUTORIAL
       {
           g.drawImage(WASD, 800/2 - WASD.getWidth()/2, 700, null);
       }
-      if(SPIELER.getXPos()==280 && SPIELER.getYPos() == 80)
+      if(SPIELER.getXPos()==320 && SPIELER.getYPos() == 80)
       {
-          g.drawImage(WASD, 800/2 - WASD.getWidth()/2, 700, null);
+          g.drawImage(T2, 800/2 - T2.getWidth()/2, 700, null);
       }
       if(SPIELER.getXPos()==440 && SPIELER.getYPos() == 80)
       {
-          g.drawImage(WASD, 800/2 - WASD.getWidth()/2, 700, null);
+          g.drawImage(T3, 800/2 - T3.getWidth()/2, 700, null);
       }
       if(SPIELER.getXPos()==400 && SPIELER.getYPos() == 240)
       {
-          g.drawImage(WASD, 800/2 - WASD.getWidth()/2, 700, null);
+          g.drawImage(T4, 800/2 - T4.getWidth()/2, 700, null);
       }
-      if(SPIELER.getXPos()==320 && SPIELER.getYPos() == 240)
+      if(SPIELER.getXPos()==360 && SPIELER.getYPos() == 240)
       {
-          g.drawImage(WASD, 800/2 - WASD.getWidth()/2, 700, null);
+          g.drawImage(T5, 800/2 - T5.getWidth()/2, 700, null);
       }
-      if(SPIELER.getXPos()==560 && SPIELER.getYPos() == 400)
+      if(SPIELER.getXPos()==520 && SPIELER.getYPos() == 400)
       {
-          g.drawImage(WASD, 800/2 - WASD.getWidth()/2, 700, null);
+          g.drawImage(T6, 800/2 - T6.getWidth()/2, 700, null);
       }
       if(SPIELER.getXPos()==480 && SPIELER.getYPos() == 560)
       {
           g.drawImage(WayBlock, 800/2 - WayBlock.getWidth()/2, 700, null);
       }
-      if(SPIELER.getXPos()==280 && SPIELER.getYPos() == 640)
+      if(SPIELER.getXPos()==360 && SPIELER.getYPos() == 640)
       {
-          g.drawImage(WASD, 800/2 - WASD.getWidth()/2, 700, null);
+          g.drawImage(T7, 800/2 - T7.getWidth()/2, 700, null);
+      }
+      if(SPIELER.getXPos()==320 && SPIELER.getYPos() == 640)
+      {
+          g.drawImage(T8, 800/2 - T8.getWidth()/2, 700, null);
       }
       if(SPIELER.getXPos()==120 && SPIELER.getYPos() == 640)
       {
-          g.drawImage(WASD, 800/2 - WASD.getWidth()/2, 700, null);
+          g.drawImage(T9, 800/2 - T9.getWidth()/2, 700, null);
       }
     
     }
